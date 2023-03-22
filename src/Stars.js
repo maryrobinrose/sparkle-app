3248df7b88a636e6a55480eea1db25eb78672f6e
Mary Robin Rose <maryrobinrose@Marys-MacBook-Pro.local>
2023-03-22 16:54:26 -0700

    add more stars

--- src/Stars.js
+++ src/Stars.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import starLarge from './images/stars/star-large.svg';
+import starMedium from './images/stars/star-medium.svg';
+import starSmall from './images/stars/star-small.svg';
 import './stars.css';
 
 const Stars = () => {
@@ -7,6 +9,8 @@ const Stars = () => {
   return (
     <div>
       <img className="star-large" src={starLarge} alt="star-large" />
+      <img className="star-medium" src={starMedium} alt="star-medium" />
+      <img className="star-small" src={starSmall} alt="star-small" />
     </div>
   );
 };
