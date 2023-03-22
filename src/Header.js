c365da48f804bcde03990162bc5faee7b323c775
Mary Robin Rose <maryrobinrose@Marys-MacBook-Pro.local>
2023-03-22 14:13:17 -0700

    header is clickable

--- src/Header.js
+++ src/Header.js
@@ -3,11 +3,11 @@ import './header.css'
 
 const Header = () => {
   return (
-    <footer>
+    <a className="header" href='/'>
       <div className="marquee-container">
         <h1 className="marquee">1-800-SPARKLE 1-800-SPARKLE 1-800-SPARKLE 1-800-SPARKLE 1-800-SPARKLE 1-800-SPARKLE </h1>
       </div>
-    </footer>
+    </a>
   );
 };
 
