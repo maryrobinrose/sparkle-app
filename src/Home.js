dbe75a1ef8e6fc11ee80a00b012fdb025aabca21
Mary Robin Rose <maryrobinrose@Marys-MacBook-Pro.local>
2023-03-22 22:00:33 -0700

    remove extra div

--- src/Home.js
+++ src/Home.js
@@ -9,17 +9,15 @@ const Home = () => {
   };
 
   return (
-    <div>
-      <div className="main-message">
-        <div className="green-square"></div>
-        <div className="pink-title"></div>
-        <div className="vertical-line"></div>
-        <div className="horizonal-line"></div>
-        <h1 className="main-message-title">Sparkle App</h1>
-        <p className="greeting-one">Welcome to Sparkle App!</p>
-        <p className="greeting-two">The #1 place to order your dreams!</p>
-        <button className="button-primary" alt="Order now" onClick={handleButtonClick}>Order Now!</button>
-      </div>
+    <div className="main-message">
+      <div className="green-square"></div>
+      <div className="pink-title"></div>
+      <div className="vertical-line"></div>
+      <div className="horizonal-line"></div>
+      <h1 className="main-message-title">Sparkle App</h1>
+      <p className="greeting-one">Welcome to Sparkle App!</p>
+      <p className="greeting-two">The #1 place to order your dreams!</p>
+      <button className="button-primary" alt="Order now" onClick={handleButtonClick}>Order Now!</button>
     </div>
   );
 };
