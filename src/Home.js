2d600358186dd4f2bacb112cc38334493b3408f0
Mary Robin Rose <maryrobinrose@Marys-MacBook-Pro.local>
2023-03-22 21:52:53 -0700

    css wip

--- src/Home.js
+++ src/Home.js
@@ -9,11 +9,17 @@ const Home = () => {
   };
 
   return (
-    <div className="main-message">
-      <h1>Sparkle App</h1>
-      <p>Welcome to Sparkle App!</p>
-      <p>The #1 place to order your dreams!</p>
-      <button className="button-primary" alt="Order now" onClick={handleButtonClick}>Order Now!</button>
+    <div>
+      <div className="main-message">
+        <div className="green-square"></div>
+        <div className="pink-title"></div>
+        <div className="vertical-line"></div>
+        <div className="horizonal-line"></div>
+        <h1 className="main-message-title">Sparkle App</h1>
+        <p className="greeting-one">Welcome to Sparkle App!</p>
+        <p className="greeting-two">The #1 place to order your dreams!</p>
+        <button className="button-primary" alt="Order now" onClick={handleButtonClick}>Order Now!</button>
+      </div>
     </div>
   );
 };
