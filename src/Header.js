8c0aa8ed20748bfff5675ab65ffb9e4bed9dcae5
Mary Robin Rose <maryrobinrose@Marys-MacBook-Pro.local>
2023-03-24 16:44:27 -0700

    remove hash

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import './header.css'
 
 const Header = () => {
   return (
-    <a className="header" href='/#'>
+    <a className="header" href='/'>
       <div className="marquee-container">
         <h1 className="marquee">1-800-SPARKLE 1-800-SPARKLE 1-800-SPARKLE 1-800-SPARKLE 1-800-SPARKLE 1-800-SPARKLE </h1>
       </div>
