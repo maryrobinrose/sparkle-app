174ae47a1d8557f47c018d55161f84b2bb136bdb
Mary Robin Rose <maryrobinrose@Marys-MacBook-Pro.local>
2023-03-28 15:43:43 -0700

    header css

--- src/Header.js
+++ src/Header.js
@@ -4,9 +4,7 @@ import './Header.css'
 const Header = () => {
   return (
     <a className="header" href='/'>
-      <div className="marquee-container">
         <h1 className="marquee">1-800-SPARKLE 1-800-SPARKLE 1-800-SPARKLE 1-800-SPARKLE 1-800-SPARKLE 1-800-SPARKLE </h1>
-      </div>
     </a>
   );
 };
